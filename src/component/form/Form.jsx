8cef4a701ba460839a9774943b81c30d3c080675
vishal007p <107975264+vishal007p@users.noreply.github.com>
2023-07-08 11:48:58 +0530

    new code

--- src/component/form/Form.jsx
+++ src/component/form/Form.jsx
@@ -1,17 +1,16 @@
-import { Box, Grid } from "@mui/material";
+import { Alert, Box, Grid, Snackbar } from "@mui/material";
 import InputField from "../InputField/InputField";
 import CommanBtn from "../CommanBtn/CommanBtn";
 import { useForm } from "react-hook-form";
 import { collection, addDoc } from "firebase/firestore";
-
-
 import { db } from "../../firebase";
+import { useState } from "react";
+
 
 const Form = () => {
 
-
-
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const [EmailMessage, setEmailMessage] = useState(false);
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
     const onSubmit = async (data, event) => {
 
@@ -21,7 +20,11 @@ const Form = () => {
             const docRef = await addDoc(collection(db, "user"), {
                 data
             });
-            alert("Data is stored in the database")
+            setEmailMessage(true)
+            setTimeout(() => {
+                setEmailMessage(false)
+            }, 6000)
+
         } catch (e) {
             console.error("Error adding document: ", e);
         }
@@ -30,20 +33,19 @@ const Form = () => {
 
 
 
-
     return (
 
         <>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form onSubmit={handleSubmit(onSubmit)} id="contact">
                 <Box sx={{ width: '100%', marginTop: '0px' }}>
                     <Grid container spacing={2} >
 
                         <Grid item xs={12} sm={6} md={6}>
-                            <InputField placeholder="Your name" type="name" label="Name" register={{ ...register("name") }} />
+                            <InputField placeholder="Name" type="name" label="Name" register={{ ...register("name") }} />
                         </Grid>
 
                         <Grid item xs={12} sm={6} md={6}>
-                            <InputField placeholder="Your email address" type="email" label="Email" register={{ ...register("email") }} />
+                            <InputField placeholder="Email" type="email" label="Email" register={{ ...register("email") }} />
                         </Grid>
 
                     </Grid>
@@ -55,11 +57,11 @@ const Form = () => {
                     <Grid container spacing={2} >
 
                         <Grid item xs={12} sm={6} md={6}>
-                            <InputField placeholder="Your phone number" type="number" label="Your phone number" register={{ ...register("number") }} />
+                            <InputField placeholder="Number" type="number" label="Your Phone Number" register={{ ...register("number") }} />
                         </Grid>
 
                         <Grid item xs={12} sm={6} md={6}>
-                            <InputField placeholder="Contact" type="text" label="Service Required" register={{ ...register("service") }} />
+                            <InputField placeholder="Service" type="text" label="Service Required" register={{ ...register("service") }} />
                         </Grid>
 
                     </Grid>
@@ -69,15 +71,23 @@ const Form = () => {
                 <Box sx={{ width: '100%', height: '150px', marginTop: '20px' }}>
                     <Grid container   >
                         <Grid item xs={12}  >
-                            <textarea name="message" type="text" cols="30" rows="10" style={{ width: '100%', height: '100%', padding: '16px 30px', border: '2px solid black', borderRadius: '5px' }} placeholder="Let us know what you need." {...register("message")}></textarea>
+                            <textarea name="message" type="text" cols="30" rows="10" style={{ width: '100%', height: '100%', padding: '16px 30px', border: '2px solid black', borderRadius: '5px' }} placeholder="Let us know what you need." {...register("message", { required: true, minLength: 20 })} />
+                            {errors.message?.type === "required" && (
+                                <p style={{ color: 'red', margin: '20px 0px' }} role="alert">hi</p>
+                            )}
                         </Grid>
                     </Grid>
 
                 </Box>
-                <Box sx={{ width: { md: '30%', sm: '60%', xs: '100%' }, marginTop: '70px', height:'50px' }}>
-                    <CommanBtn text="Send Message" />
+                <Box sx={{ width: { md: '30%', sm: '60%', xs: '100%' }, marginTop: '90px', height: '50px' }}>
+                    <CommanBtn text="Send" />
                 </Box>
             </form>
+            <Box sx={{ marginTop: '20px' }}>
+                <Snackbar open={EmailMessage} autoHideDuration={6000}  >
+                    <Alert severity="success">Sent Successfully</Alert>
+                </Snackbar>
+            </Box>
         </>
 
     );
